94fd3742d16d6950e4866147b5593a9de56c541f
Louis Cheinisse <amenobrolag@gmail.com>
2017-12-08 15:59:46 +0100

    Ajout modèles

--- game/models.py
+++ game/models.py
@@ -4,7 +4,6 @@ class TypeSpecialCard(models.Model):
     name = models.CharField(max_length=50)
 
 class Card(models.Model):
-    #id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40)
     imageSource = models.CharField(max_length=255)
     text = models.CharField(max_length=100)
@@ -15,21 +14,49 @@ class Card(models.Model):
         abstract = True
 
 class DefensiveCard(Card):
-#    id = models.OneToOneField(Card)
     value = models.FloatField()
 
 class OffensiveCard(Card):
-    #id = models.OneToOneField(Card)
     value = models.FloatField()
 
 class SpecialCard(Card):
- #   id = models.OneToOneField(Card)
     type = models.ForeignKey('TypeSpecialCard', on_delete=models.DO_NOTHING,)
     value = models.IntegerField()
 
 class CharacterCard(Card):
-   # id = models.OneToOneField(Card)
     weakness = models.CharField(max_length=20)
     strength = models.CharField(max_length=20)
     maxAlcohol = models.FloatField()
 
+class TypeStart(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class TypeDeck(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class TypeKickOff(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class TypeEnding(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class EndDeckEffect(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Parameters(models.Model):
+    deckLimit = models.IntegerField()
+    handSize = models.IntegerField()
+    cardsPerTurn = models.IntegerField()
+    cardsPerDraw = models.IntegerField()
+    turnTimer = models.IntegerField()
+    alcoholLimit = models.IntegerField()
+    typeStart = models.ForeignKey('TypeStart', on_delete=models.DO_NOTHING,)
+    typeDeck = models.ForeignKey('TypeDeck', on_delete=models.DO_NOTHING,)
+    typeKickOff = models.ForeignKey('TypeKickOff', on_delete=models.DO_NOTHING,)
+    endDeckEffect = models.ForeignKey('EndDeckEffect', on_delete=models.DO_NOTHING,)
+    typeEnding = models.ForeignKey('TypeEnding', on_delete=models.DO_NOTHING,)
