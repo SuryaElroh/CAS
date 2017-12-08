8703a4c1f46438848935396e5eba667a4e8e3a51
Louis Cheinisse <amenobrolag@gmail.com>
2017-12-08 15:11:42 +0100

    Ajout modèles Card 2

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(DefensiveCard)
+admin.site.register(OffensiveCard)
+admin.site.register(SpecialCard)
+admin.site.register(CharacterCard)
+admin.site.register(TypeSpecialCard)
--- game/models.py
+++ game/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class TypeSpecialCard(models.Model):
+    name = models.CharField(max_length=50)
+
+class Card(models.Model):
+    #id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40)
+    imageSource = models.CharField(max_length=255)
+    text = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    price = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+class DefensiveCard(Card):
+#    id = models.OneToOneField(Card)
+    value = models.FloatField()
+
+class OffensiveCard(Card):
+    #id = models.OneToOneField(Card)
+    value = models.FloatField()
+
+class SpecialCard(Card):
+ #   id = models.OneToOneField(Card)
+    type = models.ForeignKey('TypeSpecialCard', on_delete=models.DO_NOTHING,)
+    value = models.IntegerField()
+
+class CharacterCard(Card):
+   # id = models.OneToOneField(Card)
+    weakness = models.CharField(max_length=20)
+    strength = models.CharField(max_length=20)
+    maxAlcohol = models.FloatField()
+
