ba49342e9a41b1e143d56464dbb15c5b91442f48
Surya <surya.elroh@gmail.com>
2017-12-14 10:02:01 +0100

    mise à jour des models et de l'amdin

--- game/admin.py
+++ game/admin.py
@@ -3,15 +3,5 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(DefensiveCard)
-admin.site.register(OffensiveCard)
-admin.site.register(SpecialCard)
-admin.site.register(CharacterCard)
-admin.site.register(TypeSpecialCard)
-
-admin.site.register(TypeStart)
-admin.site.register(TypeDeck)
-admin.site.register(TypeKickOff)
-admin.site.register(TypeEnding)
-admin.site.register(EndDeckEffect)
-admin.site.register(Parameters)
+admin.site.register(ActionCard, ActionCardDescriptors)
+admin.site.register(CharacterCard, CharacterCardDescriptors)
